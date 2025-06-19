a5453fa54f7d0d81f2ccd0096db8531aa2752186
Catarina Morais <a044647@umaia.pt>
2025-06-19 16:59:51 +0100

    Delete src/purpleblush directory

--- src/purpleblush/src/models/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './categoria.model';
-export * from './cliente.model';
-export * from './produto.model';
-export * from './classificacao.model';
-export * from './estado.model';
-export * from './encomenda.model';
-export * from './item-encomenda.model';

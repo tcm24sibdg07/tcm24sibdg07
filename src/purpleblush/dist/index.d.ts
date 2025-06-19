db6f36c2c31389afa6fc7451ab023fa56dd9d31f
Catarina Morais <a044647@umaia.pt>
2025-06-19 19:45:23 +0100

    Add files via upload

---
+++ src/purpleblush/dist/index.d.ts
@@ -0,0 +1,3 @@
+import { ApplicationConfig, PurpleblushApplication } from './application';
+export * from './application';
+export declare function main(options?: ApplicationConfig): Promise<PurpleblushApplication>;
